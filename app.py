888b6a5219fa94ed4205a0718fee320f66af9aec
linkinwell <linkinwell@gmail.com>
2026-05-20 14:57:02 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import random
+
+# 台词列表
+words = [
+    "hello, world",
+    "你好，宝贝蛋",
+    "今天也要加油呀",
+    "你已经会写 App 了！",
+]
+
+# 页面标题
+st.title("🎤 随机说话器")
+
+# 默认显示内容
+if "talk" not in st.session_state:
+    st.session_state.talk = "点击按钮开始说话"
+
+# 按钮
+if st.button("点击说话"):
+
+    # 随机选一句
+    st.session_state.talk = random.choice(words)
+
+# 显示文字
+st.write(st.session_state.talk)
